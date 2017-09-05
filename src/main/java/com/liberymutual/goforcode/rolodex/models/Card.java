312839e9eb9f3dac96a912b862f98b3a433f328c
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-09-05 17:01:23 -0500

    Card class created

--- src/main/java/com/liberymutual/goforcode/rolodex/models/Card.java
+++ src/main/java/com/liberymutual/goforcode/rolodex/models/Card.java
@@ -1,9 +1,14 @@
 package com.liberymutual.goforcode.rolodex.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -20,4 +25,19 @@ public class Card {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 	
+	@Column(length=75, nullable = false)
+	private String firstName;
+	
+	@Column(length=75, nullable = false)
+	private String lastName;
+	
+	@Column(length=75, nullable = false)
+	private String title; 
+			
+	@Column(length=255, nullable = true)
+	private String companyName;
+	
+	private String[] addresses;
+	
+	private String[] phoneNumbers;
 }
