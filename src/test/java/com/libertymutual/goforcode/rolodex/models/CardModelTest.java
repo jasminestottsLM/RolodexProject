29b67ada067ed3c2b214b2ed5102bd5b7cd31e5b
Jasmine Stotts <Jasmine.Stotts@LibertyMutual.com>
2017-09-07 18:04:12 -0700

    fully tested card model

--- src/test/java/com/libertymutual/goforcode/rolodex/models/CardModelTest.java
+++ src/test/java/com/libertymutual/goforcode/rolodex/models/CardModelTest.java
@@ -5,10 +5,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
 
+import com.libertymutual.goforcode.rolodex.api.AddressApiController;
 import com.libertymutual.goforcode.rolodex.api.RolodexApiController;
 import com.libertymutual.goforcode.rolodex.models.Card;
 import com.libertymutual.goforcode.rolodex.repositories.AddressRepository;
@@ -17,14 +20,45 @@ import com.libertymutual.goforcode.rolodex.repositories.PhoneNumberRepository;
 
 public class CardModelTest {
 
-	
+    private CardRepository cardRepo;
+    private AddressRepository addressRepo;
+    private PhoneNumberRepository phoneRepo;
+    private AddressApiController controller;
+
+    @Before
+    public void setUp() {
+        cardRepo = mock(CardRepository.class);
+        addressRepo = mock(AddressRepository.class);
+        phoneRepo = mock(PhoneNumberRepository.class);
+    }
+  	
 	@Test
     public void test_all_gets_and_sets() {
         new BeanTester().testBean(Card.class);
     }
 
+	@Test
+	public void test_add_address_adds() {
+		Address address = addressRepo.getOne(2l);
+		Card card = new Card();
+		
+		card.addAddress(address);
+
+		assertThat(card.getAddresses()).contains(address);
+		
+	}
 	
-	
+	@Test
+	public void test_add_phone_adds() {
+		PhoneNumber phone = phoneRepo.getOne(3l);
+		Card card = new Card();
+		
+		card.addPhoneNumber(phone);
+
+		assertThat(card.getPhoneNumbers()).contains(phone);
+		
+	}
+
 	
 }
 
