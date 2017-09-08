bbf4c072710c802f73a57c212fbb5fbeda66fc8d
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-09-07 20:03:11 -0500

    added tests to phoneNumberApiController test

--- src/test/java/com/libertymutual/goforcode/rolodex/api/PhoneNumberApiControllerTests.java
+++ src/test/java/com/libertymutual/goforcode/rolodex/api/PhoneNumberApiControllerTests.java
@@ -3,6 +3,7 @@ package com.libertymutual.goforcode.rolodex.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -30,6 +31,10 @@ import com.libertymutual.goforcode.rolodex.repositories.PhoneNumberRepository;
 
 
 
+
+
+
+
 public class PhoneNumberApiControllerTests { 
 
     private CardRepository cardRepo;
@@ -45,6 +50,49 @@ public class PhoneNumberApiControllerTests {
         controller = new PhoneNumberApiController(phoneRepo, cardRepo);
     }
 
+    @Test
+	public void test_getAll_returns_all_phoneNumbers_returned_by_the_repo() {
+		// Arrange
+		ArrayList<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
+		phoneNumbers.add(new PhoneNumber());
+		phoneNumbers.add(new PhoneNumber());
+		phoneNumbers.add(new PhoneNumber());
+		
+		when(phoneRepo.findAll()).thenReturn(phoneNumbers);
+		
+		// Act
+		List<PhoneNumber> actual = controller.getAll();
+		
+		// Assert 
+		assertThat(actual.size()).isEqualTo(3);
+		assertThat(actual.get(0)).isSameAs(phoneNumbers.get(0));
+		verify(phoneRepo).findAll();
+	}
+    
+    @Test
+	public void test_getOne_returns_phoneNumber_returned_from_repo() throws StuffNotFoundException {
+		// Arrange
+		PhoneNumber phone = new PhoneNumber();
+		when(phoneRepo.findOne(2l)).thenReturn(phone);
+		
+		// Act
+		PhoneNumber actual = controller.getOne(2l);
+		
+		//Assert
+		assertThat(actual).isSameAs(phone);
+		verify(phoneRepo).findOne(2l);
+		
+	}
+       
+//    @Test
+//	public void test_getOne_throws_StuffNotFoundException_when_no_phone_returned_from_repo() {
+//		try {
+//			controller.getOne(1);
+//			
+//			fail("The controller did not throw the stuff not found exception");
+//		} catch(StuffNotFoundException snfe) {}
+//	}
+    
 	@Test
 	public void test_delete_returns_phoneNumber_deleted_when_found() {
 	Card card = new Card();
@@ -69,6 +117,32 @@ public class PhoneNumberApiControllerTests {
 	
 	}
 	
+//	@Test
+//	public void test_create_saves_and_returns_a_card() {
+//		// Arrange
+//		Card Rocky = new Card();
+//		when(cardRepo.save(Rocky)).thenReturn(Rocky);
+//				
+//		// Act
+//		Card actual = controller.create(Rocky);
+//		
+//		// Assert
+//		verify(cardRepo).save(Rocky);
+//		assertThat(Rocky).isSameAs(actual);
+//	}
 	
+	@Test
+	public void test_update_returns_phoneNumber_with_changes_made() throws StuffNotFoundException {
+		// Arrange
+		PhoneNumber phone = new PhoneNumber();
+		when(phoneRepo.save(phone)).thenReturn(phone);
+		
+		// Act
+		PhoneNumber actual = controller.update(phone, 3l);
+		
+		// Assert
+		verify(phoneRepo).save(phone);
+		assertThat(actual).isSameAs(phone);
+	}
 	
 }
